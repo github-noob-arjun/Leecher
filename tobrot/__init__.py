b56296d9d61926cf9fc81e0d2e857704680c5877
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-22 10:11:53 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -52,9 +52,9 @@ for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
 
 # The Telegram API things
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "12345"))
+APP_ID = int(os.environ.get("APP_ID", "4738674"))
 API_HASH = os.environ.get("API_HASH")
-OWNER_ID = int(os.environ.get("OWNER_ID", "539295917"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1266733572"))
 
 # Get these values from my.telegram.org
 # to store the channel ID who are authorized to use the bot
